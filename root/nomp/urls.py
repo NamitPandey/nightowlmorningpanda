13ebb083b17c51ac299cd1abd8dc2b167f022093
NamitPandey <nix.pandey@gmail.com>
2026-01-16 13:07:21 +0530

    added resume page and skill page. fixed typed animation

--- root/nomp/urls.py
+++ root/nomp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views as views
 # from django.contrib.auth import views as auth_views
 
-app_name="heena"
+app_name="nomp"
 
 urlpatterns = [
-    path("",views.home, name="heena_home"), 
+    path("",views.home, name="home"), 
+    path("resume",views.resume, name="resume"), 
+    path("skill",views.skill, name="skill"), 
 ]
--- root/nomp/views.py
+++ root/nomp/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def home(request):
     
     return render(request,"nomp/home.html",context={})
+
+def resume(request):
+
+    return render(request, "nomp/resume.html", context={})
+
+def skill(request):
+
+    return render(request, "nomp/skill.html", context={})
